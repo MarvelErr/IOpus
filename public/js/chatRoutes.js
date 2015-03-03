e9578cb1bb9d6f8d68aa0c794925fad7585f59b7
luwenxu <992273307@qq.com>
2015-03-03 10:59:03 +0800

    version-0.1

---
+++ public/js/chatRoutes.js
@@ -0,0 +1,45 @@
+/**
+ * Created by luwenxu on 2015/3/3.
+ */
+app.config(function($stateProvider){
+    $stateProvider.state('login',{
+        templateUrl:'/chatRoom/login.html',
+        url:'/login',
+        controller:function($scope,$state){
+            $scope.confirm=function(){
+                window.localStorage.IOpusUser=($scope.nickname!=null&&$scope.nickname!='')?$scope.nickname:null;
+                $state.go('chat');
+            }
+        }
+    }).state('chat',{
+        templateUrl:'/chatRoom/chat.html',
+        url:'/chat',
+        controller:function($scope,socket,$state){
+            if(!window.localStorage.IOpusUser){
+                $state.go('login');
+            }else{
+                $scope.messages=[];
+                socket.on('messageAdded',function(msgs){
+                    $scope.$apply(
+                        function(){
+                            $scope.messages=msgs;
+                        }
+                    )
+                });
+                socket.on('allMessages',function(msgs){
+                    $scope.$apply(
+                        function(){
+                            $scope.messages=msgs;
+                        }
+                    )
+                });
+                $scope.send=function(){
+                    if($scope.msg){
+                        socket.emit('newMessage',window.localStorage.IOpusUser+':'+$scope.msg);
+                        $scope.msg=null;
+                    }
+                }
+            }
+        }
+    })
+});
