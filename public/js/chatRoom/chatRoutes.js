bc8125d42b2cdc5d4d3a0504ef535c6e4f3c467f
luwenxu <992273307@qq.com>
2015-04-13 16:46:32 +0800

    点赞和聊天室

--- public/js/chatRoom/chatRoutes.js
+++ public/js/chatRoom/chatRoutes.js
@@ -1,47 +1,45 @@
 /**
  * Created by luwenxu on 2015/3/3.
  */
-app.config(function($stateProvider){
-    $stateProvider.state('login',{
-        templateUrl:'/chatRoom/login.html',
-        url:'/login',
-        controller:function($scope,$state){
-            $scope.confirm=function($event){
-                if($event.keyCode==13){
-                    window.localStorage.IOpusUser=($scope.nickname!=null&&$scope.nickname!='')?$scope.nickname:null;
+app.config(function ($stateProvider) {
+    $stateProvider.state('login', {
+        templateUrl: '/chatRoom/login.html',
+        url: '/login',
+        controller: function ($scope, $state) {
+            $scope.confirm = function ($event) {
+                if ($event.keyCode == 13) {
+                    window.localStorage.IOpusUser = ($scope.nickname != null && $scope.nickname != '') ? $scope.nickname : null;
                     $state.go('chat');
                 }
             };
         }
-    }).state('chat',{
-        templateUrl:'/chatRoom/chat.html',
-        url:'/chat',
-        controller:function($scope,socket,$state){
-            if(!window.localStorage.IOpusUser){
-                $state.go('login');
-            }else{
-                $scope.messages=[];
-                socket.on('messageAdded',function(msgs){
+    }).state('chat', {
+        templateUrl: '/chatRoom/chat.html',
+        url: '/chat',
+        controller: function ($scope, socket, $state) {
+            {
+                $scope.messages = [];
+                socket.on('messageAdded', function (msgs) {
                     $scope.$apply(
-                        function(){
-                            $scope.messages=msgs;
+                        function () {
+                            $scope.messages = msgs;
                         }
                     )
                 });
-                socket.on('allMessages',function(msgs){
+                socket.on('allMessages', function (msgs) {
                     $scope.$apply(
-                        function(){
-                            $scope.messages=msgs;
+                        function () {
+                            $scope.messages = msgs;
                         }
                     )
                 });
-                $scope.send=function(){
-                    if($scope.msg){
-                        socket.emit('newMessage',{
-                            name:window.localStorage.IOpusUser,
-                            msg:$scope.msg
+                $scope.send = function () {
+                    if ($scope.msg) {
+                        socket.emit('newMessage', {
+                            name: window.localStorage.IOpusUser,
+                            msg: $scope.msg
                         });
-                        $scope.msg=null;
+                        $scope.msg = null;
                     }
                 }
             }
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,16 @@
 /**
  * Created by luwenxull on 2015/3/26.
  */
-$(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-    $('.fa-thumbs-o-up').click(function(){
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.fa-thumbs-o-up').click(function () {
         /*$.ajax({
-            url:'/thumbs'
-        }).done(function(data){console.log(data)})*/
-        console.log(parseInt($('.up').text()+1));
-    })
+         url:'/thumbs'
+         }).done(function(data){console.log(data)})*/
+        //console.log();
+        var count=parseInt($('.up').text());
+        count++;
+        $('.up').text(count);
+        swal("Good job!", "Thanks for your support!", "success");
+    });
 });
